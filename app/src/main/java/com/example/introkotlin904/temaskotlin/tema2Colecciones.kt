d003e303718db761b33d19695f23c545ef85957d
IDGS-904-21000403 <21000403@alumnos.utleon.edu.mx>
2025-05-06 18:41:35 -0600

    clase 1

---
+++ app/src/main/java/com/example/introkotlin904/temaskotlin/tema2Colecciones.kt
@@ -0,0 +1,27 @@
+package com.example.introkotlin904.temaskotlin
+/*
+    List
+    Mutablelist
+    Set
+    MutableSet
+    Map
+    MitableMap
+
+
+    val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
+        println(readOnlyFiguras)
+
+    val figura: mutableFiguras = mutableListOf("cuadrado","triangulo","circulo")
+    val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
+    val mutableFiguras = mutableListOf("cuadrado","triangulo","circulo")
+
+    val frutas = setOf("manzana", "bananas", "naranja")
+    val frutas = mutableSetOf("manzana", "bananas", "naranja")
+
+
+
+*/fun main(){
+    val coches = mapOf("uno" to 1, "dos" to 2, "tres" to 3)
+    val coches2 = mutableMapOf("uno" to 1, "dos" to 2, "tres" to 3)
+    println(coches2)
+}
---
+++ app/src/main/java/com/example/introkotlin904/temaskotlin/variables.kt
@@ -0,0 +1,21 @@
+package com.example.introkotlin904.temasKotlin
+
+//definir funciones
+fun main () {
+    // variables solo lectura val
+    // variables mutables var
+
+    var a = 4
+    var b = 8
+    var c = 10
+
+    println(a)
+    println("el valor de b es $b")
+    println("la suma es ${c+3}")
+
+    val num1:Int= 24
+    var nombre: String="Edgar"
+    var caracter:Char='C'
+    var num2: Float=12.5f
+    var num3: Double=12.5
+}
