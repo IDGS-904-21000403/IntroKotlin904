4a94c415da4e40893d4d2ee4e080db33096b8d86
IDGS-904-21000403 <21000403@alumnos.utleon.edu.mx>
2025-05-06 19:35:50 -0600

    tema3 if-when

--- app/src/main/java/com/example/introkotlin904/temaskotlin/tema2Colecciones.kt
+++ app/src/main/java/com/example/introkotlin904/temaskotlin/tema2Colecciones.kt
@@ -8,8 +8,7 @@ package com.example.introkotlin904.temaskotlin
     MitableMap
 
 
-    val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
-        println(readOnlyFiguras)
+
 
     val figura: mutableFiguras = mutableListOf("cuadrado","triangulo","circulo")
     val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
@@ -21,7 +20,20 @@ package com.example.introkotlin904.temaskotlin
 
 
 */fun main(){
-    val coches = mapOf("uno" to 1, "dos" to 2, "tres" to 3)
+  /*  val coches = mapOf("uno" to 1, "dos" to 2, "tres" to 3)
     val coches2 = mutableMapOf("uno" to 1, "dos" to 2, "tres" to 3)
-    println(coches2)
+    println(coches2)*/
+
+    val readOnlyFiguras = listOf("cuadrado","triangulo","circulo")
+    println(readOnlyFiguras)
+    println(" la primera figura es ${readOnlyFiguras[0]}")
+    println("el primer elemento es ${readOnlyFiguras.first()}")
+    println("numero de elementos ${readOnlyFiguras.count()} items")
+    println("circulo" in readOnlyFiguras)
+    //readOnlyFiguras.add("pentagono")
+    var figura : MutableList<String> = mutableListOf("cuadrado2","triangulo2","circulo2")
+    figura.add("pentagono2")
+    println(figura)
+    figura.remove("cuadrado2")
+    println(figura)
 }
